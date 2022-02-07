b9a9090b3ec10b74234919de8f13e53d4051d7f8
rostiarso <rostiarso@gmail.com>
2022-02-07 22:27:06 +0100

    added progress bar, right align table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 
@@ -50,6 +51,7 @@ import { HandelsbenamingService } from './handelsbenaming.service';
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatProgressBarModule,
 
     AppRoutingModule
 
